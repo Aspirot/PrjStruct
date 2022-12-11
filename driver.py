bfbf302a175b81c8ee388aa7358e0430a1476e4a
Aspirot <81372169+Aspirot@users.noreply.github.com>
2022-12-11 18:39:13 -0500

    Optimization des if else

--- driver.py
+++ driver.py
@@ -36,6 +36,7 @@ if __name__ == '__main__':
         if current_task is None:
             if TaskPriorityQueue.size() > 1:
                 current_task = TaskPriorityQueue.dequeue()
+                print("Starting " + str(current_task))
                 time.sleep(1)
                 current_task.execution_time -= 1
                 print("Running " + str(current_task))
@@ -45,35 +46,20 @@ if __name__ == '__main__':
         else:
             if current_task.execution_time == 0:
                 print("Finished " + str(current_task))
-                if OnHoldStack.is_empty():
-                    if TaskPriorityQueue.size() == 1:
-                        current_task = None
-                        time.sleep(1)
-                        print("No task at the moment")
-                    else:
-                        current_task = TaskPriorityQueue.dequeue()
-                        print("Starting " + str(current_task))
-                        time.sleep(1)
-                        current_task.execution_time -= 1
-                        print("Running " + str(current_task))
+                if OnHoldStack.is_empty() and TaskPriorityQueue.size() == 1:
+                    current_task = None
+                elif OnHoldStack.is_empty():
+                    current_task = TaskPriorityQueue.dequeue()
+                    print("Starting " + str(current_task))
                 elif TaskPriorityQueue.size() == 1:
                     current_task = OnHoldStack.pop()
                     print("Starting " + str(current_task))
-                    time.sleep(1)
-                    current_task.execution_time -= 1
-                    print("Running " + str(current_task))
                 elif TaskPriorityQueue.first() < OnHoldStack.peek():
                     current_task = TaskPriorityQueue.dequeue()
                     print("Starting " + str(current_task))
-                    time.sleep(1)
-                    current_task.execution_time -= 1
-                    print("Running " + str(current_task))
                 else:
                     current_task = OnHoldStack.pop()
                     print("Starting " + str(current_task))
-                    time.sleep(1)
-                    current_task.execution_time -= 1
-                    print("Running " + str(current_task))
             else:
                 if TaskPriorityQueue.size() > 1:
                     if TaskPriorityQueue.first() < current_task:
@@ -81,17 +67,13 @@ if __name__ == '__main__':
                         OnHoldStack.push(current_task)
                         current_task = TaskPriorityQueue.dequeue()
                         print("Starting " + str(current_task))
-                        time.sleep(1)
-                        current_task.execution_time -= 1
-                        print("Running " + str(current_task))
-                    else:
-                        time.sleep(1)
-                        current_task.execution_time -= 1
-                        print("Running " + str(current_task))
-                else:
-                    time.sleep(1)
-                    current_task.execution_time -= 1
-                    print("Running " + str(current_task))
+            if current_task is None:
+                time.sleep(1)
+                print("No task at the moment")
+            else:
+                time.sleep(1)
+                current_task.execution_time -= 1
+                print("Running " + str(current_task))
         time_elapsed += 1
         time_left -= 1
     print("Process is terminated!")
